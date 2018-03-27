9d13a57c1f4b96885b9ddf78479784477e6b79ff
ETBE <ebernoux@octo.com>
2018-03-27 16:19:29 +0200

    correction

--- app/httpService.js
+++ app/httpService.js
@@ -3,16 +3,15 @@ const http = require("http");
 const USER_AGENT = process.env.USER_AGENT || null
 
 function get (url) {
-	console.log(USER_AGENT)
 	const options = {
   		url: url,
   		headers: {
     		'User-Agent': USER_AGENT
   		}
 	}
+	console.log(options)
 	http.get(options, res => {
   		res.setEncoding("utf8");
-  		res.
   		res.on("data", data => {
     		console.log(`response:${data}`)
   		})
