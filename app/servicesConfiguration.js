bf8c362879df598eb41e76d7ede81dadc0d7a0fc
ETBE <ebernoux@octo.com>
2018-04-06 11:25:26 +0200

    deco-all

--- app/servicesConfiguration.js
+++ app/servicesConfiguration.js
@@ -1,11 +1,20 @@
+const MONOLYTHE_DNS = process.env.MONOLYTHE_DNS|| "localhost"
+const MONOLYTHE_PORT = process.env.MONOLYTHE_PORT || 3000
+
 const VIREMENT_EXTERNE_DNS = process.env.VIREMENT_EXTERNE_DNS || "localhost"
 const VIREMENT_EXTERNE_PORT = process.env.VIREMENT_EXTERNE_PORT || 3000
 
 const VIREMENT_INTERNE_DNS = process.env.VIREMENT_INTERNE_DNS || "localhost"
 const VIREMENT_INTERNE_PORT = process.env.VIREMENT_INTERNE_PORT || 3000
 
-const MONOLYTHE_DNS = process.env.MONOLYTHE_DNS|| "localhost"
-const MONOLYTHE_PORT = process.env.MONOLYTHE_PORT || 3000
+const CONSULTATION_SOLDE_DNS = process.env.CONSULTATION_SOLDE_DNS || "localhost"
+const CONSULTATION_SOLDE_PORT = process.env.CONSULTATION_SOLDE_PORT || 3000
+
+const OPPOSITION_CARTE_DNS = process.env.OPPOSITION_CARTE_DNS || "localhost"
+const OPPOSITION_CARTE_PORT = process.env.OPPOSITION_CARTE_PORT || 3000
+
+const PLAFOND_CARTE_DNS = process.env.PLAFOND_CARTE_DNS || "localhost"
+const PLAFOND_CARTE_PORT = process.env.PLAFOND_CARTE_PORT || 3000
 
 module.exports =  {
 	virement_externe:{
@@ -26,21 +35,21 @@ module.exports =  {
 	},
 	opposition_carte: {
 		operation: "get",
-		host:MONOLYTHE_DNS,
-		port:MONOLYTHE_PORT,
-		route: `/api/carteService/${Math.floor(Math.random()*100000000)}/opposition/${Math.floor(Math.random()*100000000)}`
+		host:CONSULTATION_SOLDE_DNS,
+		port:CONSULTATION_SOLDE_PORT,
+		route: "/api/V1/consultation_solde"
 	},
 	consultation_solde: {
 		opperation: "get",
-		host:MONOLYTHE_DNS,
-		port:MONOLYTHE_PORT,
-		route: `/services/consultation_solde/${Math.floor(Math.random()*100000000)}`
+		host:OPPOSITION_CARTE_DNS,
+		port:OPPOSITION_CARTE_PORT,
+		route: "/api/V1/opposition_carte"
 	},
 	plafond_carte: {
 		opperation: "get",
-		host:MONOLYTHE_DNS,
-		port:MONOLYTHE_PORT,
-		route: "/XMLrequestConsumer",
+		host:PLAFOND_CARTE_DNS,
+		port:PLAFOND_CARTE_PORT,
+		route: "/api/V1/plafond_carte",
 		body: `
 			<?xml version="1.0" ?>
 			<Request
